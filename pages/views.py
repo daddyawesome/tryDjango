cc2014f449561afe2f5cf0346fab31a6d3cd4f70
daddyawesome <sablay296@gmail.com>
2020-01-08 18:37:20 +0000

    add templates to settings

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(*args, **kwargs): # *args, **kwargs
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 def contact_view(*args, **kwargs): # *args, **kwargs
     return HttpResponse("<h1>Contact Page</h1>")
