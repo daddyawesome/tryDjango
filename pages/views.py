83403d81d153c0415111176235bd451406847e69
daddyawesome <sablay296@gmail.com>
2020-01-08 22:54:28 +0000

    created view on product

--- pages/views.py
+++ pages/views.py
@@ -9,7 +9,13 @@ def contact_view(request, *args, **kwargs): # *args, **kwargs
     return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs): # *args, **kwargs
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text" : "This is About Us",
+        "my_number" : 9166190483,
+        "my_list" : [123, 456, 789 ,'abc']
+
+    }
+    return render(request, "about.html", my_context)
 
 def social_view(*args, **kwargs): # *args, **kwargs
     return HttpResponse("<h1>Social Page</h1>")
