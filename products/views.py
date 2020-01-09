f99f5f6f1b9e753ff38998913f283c73a9b3b76f
daddyawesome <sablay296@gmail.com>
2020-01-09 17:06:43 +0000

    updating view on form

--- products/views.py
+++ products/views.py
@@ -6,9 +6,13 @@ from .models import Product
 # Create your views here.
 
 def product_create_view(request):
-    form = RawProductForm()
+    my_form = RawProductForm()
+    if request.method == "POST":
+         my_form = RawProductForm(request.POST)
+         if my_form.is_valid():
+             # now the data is good
     context = {
-        "form": form
+        "form": my_form
     }
     return render (request, "products/product_create.html", context)
 
