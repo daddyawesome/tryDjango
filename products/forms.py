5465d4092059caf01e6527c2a94ad14513c04c22
daddyawesome <sablay296@gmail.com>
2020-01-09 19:55:20 +0000

    urls is added for products

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,22 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title           = forms.CharField(label='',
+                        widget=forms.TextInput(attrs={"placeholder":"Your Title"}))
+    description     = forms.CharField(
+                            required=False,
+                            widget=forms.Textarea(
+                                attrs={
+                                    "placeholder":"Your description",
+                                    "class":"new-class-name two",
+                                    "id":"my-id-for-textarea",
+                                    "rows":20,
+                                    "cols":120
+                                    }
+                                )
+                            )
+    price           = forms.DecimalField(initial=199.99)
+
     class Meta:
         model = Product
         fields = [
@@ -13,6 +29,17 @@ class ProductForm(forms.ModelForm):
             ]
 
 class RawProductForm(forms.Form):
-    title           = forms.CharField()
-    descripttion    = forms.CharField()
-    price           = forms.DecimalField()
+    title           = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder":"Your Title"}))
+    description     = forms.CharField(
+                            required=False,
+                            widget=forms.Textarea(
+                                attrs={
+                                    "placeholder":"Your description",
+                                    "class":"new-class-name two",
+                                    "id":"my-id-for-textarea",
+                                    "rows":20,
+                                    "cols":120
+                                    }
+                                )
+                            )
+    price           = forms.DecimalField(initial=199.99)
--- products/views.py
+++ products/views.py
@@ -1,43 +1,51 @@
-from django.shortcuts import render
-
-from .forms import ProductForm, RawProductForm
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ProductForm
 from .models import Product
-# Create your views here.
+
 
 def product_create_view(request):
-    my_form = RawProductForm()
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+
+def product_update_view(request, id=id):
+    obj = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+
+def product_list_view(request):
+    queryset = Product.objects.all() # list of objects
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "products/product_list.html", context)
+
+def product_detail_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    context = {
+        "object": obj
+    }
+    return render(request, "products/product_detail.html", context)
+
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
     if request.method == "POST":
-         my_form = RawProductForm(request.POST)
-         if my_form.is_valid():
-             # now the data is good
+        obj.delete()
+        return redirect('../../')
     context = {
-        "form": my_form
+        "object": obj
     }
-    return render (request, "products/product_create.html", context)
-
-#def product_create_view(request):
-#    form = ProductForm(request.POST or None)
-#    if form.is_valid():
-#        form.save()
-#        form = ProductForm()
-#
-#    context = {
-#        'form': form
-#    }
-#    return render (request, "products/product_create.html", context)
-
-
-
-
-def product_detail_view(request):
-    obj = Product.objects.get(id=3)
-        #context = {
-        #    'title': obj.title,
-        #    'description': obj.description,
-        #    'price': obj.price,
-        #}
-    context = {
-        'object': obj,
-    }
-    return render (request, "products/product_details.html", context)
+    return render(request, "products/product_delete.html", context)
